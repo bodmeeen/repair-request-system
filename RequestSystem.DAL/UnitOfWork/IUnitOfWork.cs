9afda3a0d9578d1745caca619a163b8f4a311785
Maksym Demianii <maks.demianp@gmail.com>
2025-07-03 08:21:06 +0300

    Implementation of Unit of Work

---
+++ RequestSystem.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using RequestSystem.DAL.Repositories.Interfaces;
+
+namespace RequestSystem.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        IDeviceRepository Devices { get; }
+        IRepairRequestRepository RepairRequests { get; }
+        ICommentRepository Comments { get; }
+
+        Task<int> CommitAsync();
+    }
+}
---
+++ RequestSystem.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using RequestSystem.DAL.Data;
+using RequestSystem.DAL.Repositories;
+using RequestSystem.DAL.Repositories.Interfaces;
+
+namespace RequestSystem.DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDBContext _context;
+
+        public IUserRepository Users { get; }
+        public IDeviceRepository Devices { get; }
+        public IRepairRequestRepository RepairRequests { get; }
+        public ICommentRepository Comments { get; }
+
+        public UnitOfWork(ApplicationDBContext context,
+                          IUserRepository userRepository,
+                          IDeviceRepository deviceRepository,
+                          IRepairRequestRepository repairRequestRepository,
+                          ICommentRepository commentRepository)
+        {
+            _context = context;
+            Users = userRepository;
+            Devices = deviceRepository;
+            RepairRequests = repairRequestRepository;
+            Comments = commentRepository;
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
